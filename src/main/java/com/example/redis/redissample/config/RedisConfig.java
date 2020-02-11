91f2ac351460c60187cf88c08e319ddab0b53b2e
Sumit <sumit.kaggarwal@gmail.com>
2020-02-11 13:06:50 +0530

    Redis Sample

--- src/main/java/com/example/redis/redissample/config/RedisConfig.java
+++ src/main/java/com/example/redis/redissample/config/RedisConfig.java
@@ -1,19 +1,37 @@
 package com.example.redis.redissample.config;
 
 import com.example.redis.redissample.model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
+
+    @Autowired
+    ClusterConfigurationProperties clusterProperties;
+
+    public @Bean
+    RedisConnectionFactory connectionFactory() {
+
+        return new JedisConnectionFactory(
+                new RedisClusterConfiguration(clusterProperties.getNodes()));
+    }
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
         RedisProperties properties = redisProperties();
@@ -27,8 +45,8 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, Item> redisTemplate() {
         final RedisTemplate<String, Item> template = new RedisTemplate<String, Item>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
+        template.setConnectionFactory(connectionFactory());
+        // template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
         return template;
     }
 
